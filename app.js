a61c930d29deb777dbecbb3e4add4a4251ec5f6f
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-14 10:31:38 +0530

    added text area and button

---
+++ app.js
@@ -0,0 +1,6 @@
+var translatebtn = document.querySelector("#btn-translate");
+
+function DoTranslation() {
+    console.log("Translated text will appear here.");
+}
+translatebtn.addEventListener("click",DoTranslation);
