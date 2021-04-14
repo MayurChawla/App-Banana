8da173a7cb9b646c52504e2233d839d54d21a7cf
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-14 19:43:06 +0530

    done

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ var translatebtn = document.querySelector("#btn-translate");
 var inputtxt = document.querySelector("#input-txtarea");
 var outputtxt = document.querySelector("#output-txtarea");
 
+//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json?text=";
+
+function getTranslationURL(text) {
+    return (serverUrl + text);
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server..! Try again after Sometime");
+}
+
 function DoTranslation() {
-    outputtxt.innerHTML = inputtxt.value;
+    var usertextinp = inputtxt.value;
+    // outputtxt.innerHTML = inputtxt.value;
+    fetch(getTranslationURL(usertextinp))
+        .then(response => response.json())
+        .then(json => {
+            var translatedtextoutput = json.contents.translated;
+            outputtxt.innerText = translatedtextoutput;
+        })
+        .cache(errorHandler)    
+        // console.log(json.contents.translated)
 }
 translatebtn.addEventListener("click",DoTranslation);
